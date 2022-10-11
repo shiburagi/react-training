71f6f8e485caa09accfb000c946d95480fbb6e22
zariman <tr32010@gmail.com>
2022-10-11 19:12:20 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import { AppBar, MenuItem, Select, Toolbar, useTheme } from '@material-ui/core';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import './App.css';
+import Axios from 'axios'
 
-function App() {
+const callImage = async () => {
+  const result = await Axios.get(
+    "https://jsonplaceholder.typicode.com/photos")
+  return result?.data ?? []
+}
+const useImages = () => {
+  const [images, setImages] = useState()
+  useEffect(() => {
+    callImage().then(images => {
+      setImages(images.slice(0, 20))
+    })
+  }, [])
+
+  return images
+}
+
+const App = () => {
+  // access theme
+  const { palette } = useTheme()
+  const { primary, secondary } = palette
+
+  const [title, setTitle] = useState("My Head")
+  //assign color dari theme
+  const [color, setColor] = useState(primary.main)
+
+  const date = new Date()
+  const dateRef = useRef(date)
+
+  if (title === "Redirect") dateRef.current = date
+
+  // retrive images
+  const images = useImages()
+  const [selectedImage, setSelectedImage] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title} color={color} sticky={true}
+        counter={100} />
+      {date.toISOString()}
+      <br />
+      {dateRef.current.toISOString()}
+      <br />
+      <button onClick={e => { setTitle("Redirect") }}>change title</button>
+      <button onClick={e => { setColor(secondary.main) }}>change color</button>
+      <br />
+      {selectedImage ?
+        <img src={selectedImage?.thumbnailUrl} alt="ppp"/> : ""}
+      <br />
+      <Select
+        displayEmpty
+        value={selectedImage?.id ?? ""}
+        onChange={e => {
+          const value = e.target.value
+          const image = images?.find(({ id }) => id === value)
+          setSelectedImage(image)
+        }}>
+        {
+          images?.map(({ id, title }) =>
+            <MenuItem key={id} value={id}>{title}</MenuItem>)
+        }
+      </Select>
     </div>
-  );
+  )
+}
+
+
+const Header = (props) => {
+  const { title, color = "#000000" } = props
+  const style = {
+    backgroundColor: color,
+  }
+  return (
+    <>
+      <AppBar>
+        <Toolbar style={style}>
+          {title}
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, MuiThemeProvider } from '@material-ui/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#E74C3C"
+    },
+    secondary: {
+      main: "#9B59B6"
+    }
+  }
+})
 root.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </MuiThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
